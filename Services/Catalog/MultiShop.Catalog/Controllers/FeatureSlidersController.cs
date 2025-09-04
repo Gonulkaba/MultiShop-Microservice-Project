1974d775d78201a51554686bc5460f6fca1d7f7d
Gönül Kaba <gonul.kaba123@gmail.com>
2025-09-04 22:42:37 +0300

    feat: implemented Feature Slider API and admin CRUD with home page integration

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/FeatureSlidersController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.FeatureSliderDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Services.FeatureSliderServices;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeatureSlidersController : Controller
+    {
+        private readonly IFeatureSliderService _FeatureSliderService;
+        public FeatureSlidersController(IFeatureSliderService FeatureSliderService)
+        {
+            _FeatureSliderService = FeatureSliderService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> FeatureSliderList()
+        {
+            var values = await _FeatureSliderService.GetAllFeatureSliderAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFeatureSliderById(string id)
+        {
+            var values = await _FeatureSliderService.GetByIdFeatureSliderAsync(id);
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateFeatureSlider(CreateFeatureSliderDto createFeatureSliderDto)
+        {
+            await _FeatureSliderService.CreateFeatureSliderAsync(createFeatureSliderDto);
+            return Ok("Öne çıkan görsel Başarıyla Eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteteFeatureSlider(string id)
+        {
+            await _FeatureSliderService.DeleteFeatureSliderAsync(id);
+            return Ok("Öne çıkan görsel Başarıyla Silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateFeatureSlider(UpdateFeatureSliderDto updateFeatureSliderDto)
+        {
+            await _FeatureSliderService.UpdateFeatureSliderAsync(updateFeatureSliderDto);
+            return Ok("Öne çıkan görsel Başarıyla Güncellendi");
+        }
+    }
+}
