da525c9622496d3e89094aa8923c46dbd0fdab08
Gönül Kaba <gonul.kaba123@gmail.com>
2025-09-19 23:24:39 +0300

    feat: implemented product detail page including API services and admin UI CRUD

--- Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -10,8 +10,9 @@ namespace MultiShop.WebUI.Controllers
             ViewBag.i = id;
             return View();
         }
-        public IActionResult ProductDetail()
+        public IActionResult ProductDetail(string id)
         {
+            ViewBag.x = id;
             return View();
         }
     }
